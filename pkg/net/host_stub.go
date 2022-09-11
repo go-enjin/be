2827b461fdbf158dc401f92da4cfb71c2aaa7f7f
Kevin C. Krinke <kevin@krinke.ca>
2022-09-11 14:52:45 -0400

    pkg/net: go:build fixes for host stub/unix

--- pkg/net/host_stub.go
+++ pkg/net/host_stub.go
@@ -1,4 +1,4 @@
-//go:build !linux
+//go:build !linux && !darwin
 
 // Copyright (c) 2022  The Go-Enjin Authors
 //
--- pkg/net/host_unix.go
+++ pkg/net/host_unix.go
@@ -1,3 +1,5 @@
+//go:build linux || darwin
+
 // Copyright (c) 2022  The Go-Enjin Authors
 //
 // Licensed under the Apache License, Version 2.0 (the "License");
