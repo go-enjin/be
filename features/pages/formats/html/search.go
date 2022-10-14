e0bf80f1569fde60b440daac3c751bf2716cc71f
Kevin C. Krinke <kevin@krinke.ca>
2022-10-14 18:47:26 -0400

    features/pages/formats/html: new html document type

---
+++ features/pages/formats/html/search.go
@@ -0,0 +1,53 @@
+// Copyright (c) 2022  The Go-Enjin Authors
+//
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+//     http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+
+package html
+
+import (
+	"github.com/blevesearch/bleve/v2/analysis/analyzer/simple"
+	"github.com/blevesearch/bleve/v2/mapping"
+
+	"github.com/go-enjin/be/pkg/search"
+)
+
+var _ Document = (*CDocument)(nil)
+
+type Document interface {
+	search.Document
+	AddHeading(text string)
+}
+
+type CDocument struct {
+	search.CDocument
+
+	Headings []string `json:"headings"`
+}
+
+func NewHtmlDocument(url, title string) (doc *CDocument) {
+	doc = new(CDocument)
+	doc.Type = "html"
+	doc.Title = title
+	doc.Url = url
+	return
+}
+
+func (d *CDocument) AddHeading(text string) {
+	d.Headings = append(d.Headings, text)
+}
+
+func NewHtmlDocumentMapping() (dm *mapping.DocumentMapping) {
+	dm = search.NewDocumentMapping()
+	dm.AddFieldMappingsAt("Headings", search.NewDefaultTextFieldMapping(simple.Name))
+	return
+}
