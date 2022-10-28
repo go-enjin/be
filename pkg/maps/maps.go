e3639929e2c3b51f972dca8826b8afb30e155ad8
Kevin C. Krinke <kevin@krinke.ca>
2022-10-27 22:11:33 -0400

    pkg/maps: added ReverseSortedKeys

--- pkg/maps/maps.go
+++ pkg/maps/maps.go
@@ -192,6 +192,14 @@ func SortedKeys[V interface{}](data map[string]V) (keys []string) {
 	return
 }
 
+func ReverseSortedKeys[V interface{}](data map[string]V) (keys []string) {
+	for key, _ := range data {
+		keys = append(keys, key)
+	}
+	sort.Sort(sort.Reverse(sortorder.Natural(keys)))
+	return
+}
+
 func CamelizeKeys[V interface{}](data map[string]V) (camelized map[string]V) {
 	camelized = make(map[string]V)
 	for k, v := range data {
