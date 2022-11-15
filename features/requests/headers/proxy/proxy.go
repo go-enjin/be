cff3c3fcd1fc99d5b7363250cef6a44c1e47fd2d
Kevin C. Krinke <kevin@krinke.ca>
2022-11-15 12:31:11 -0500

    features/requests/headers/proxy: refactored for formalized feature structure; use chi middleware.RealIP to do the actual work

--- features/requests/headers/proxy/proxy.go
+++ features/requests/headers/proxy/proxy.go
@@ -19,50 +19,58 @@ package proxy
 import (
 	"net/http"
 
+	"github.com/go-chi/chi/v5/middleware"
 	"github.com/urfave/cli/v2"
 
 	"github.com/go-enjin/be/pkg/feature"
-	"github.com/go-enjin/be/pkg/log"
-	"github.com/go-enjin/be/pkg/net"
 )
 
 var (
-	_ feature.Feature         = (*Feature)(nil)
-	_ feature.RequestModifier = (*Feature)(nil)
+	_ Feature            = (*CFeature)(nil)
+	_ MakeFeature        = (*CFeature)(nil)
+	_ feature.Middleware = (*CFeature)(nil)
 )
 
 const Tag feature.Tag = "HeaderProxy"
 
-type Feature struct {
-	feature.CFeature
+type Feature interface {
+	feature.Middleware
+}
+
+type CFeature struct {
+	feature.CMiddleware
 
 	enabled bool
 }
 
 type MakeFeature interface {
-	feature.MakeFeature
-
 	Enable() MakeFeature
+
+	Make() Feature
 }
 
 func New() MakeFeature {
-	f := new(Feature)
+	f := new(CFeature)
 	f.Init(f)
 	return f
 }
 
 // Enable sets the default state to enabled, overridden by --header-proxy
-func (f *Feature) Enable() MakeFeature {
+func (f *CFeature) Enable() MakeFeature {
 	f.enabled = true
 	return f
 }
 
-func (f *Feature) Tag() (tag feature.Tag) {
+func (f *CFeature) Make() Feature {
+	return f
+}
+
+func (f *CFeature) Tag() (tag feature.Tag) {
 	tag = Tag
 	return
 }
 
-func (f *Feature) Build(b feature.Buildable) (err error) {
+func (f *CFeature) Build(b feature.Buildable) (err error) {
 	b.AddFlags(&cli.BoolFlag{
 		Name:    "header-proxy",
 		Usage:   "rewrite req.RemoteAddr to take proxy headers into account",
@@ -71,22 +79,20 @@ func (f *Feature) Build(b feature.Buildable) (err error) {
 	return
 }
 
-func (f *Feature) Startup(ctx *cli.Context) (err error) {
+func (f *CFeature) Startup(ctx *cli.Context) (err error) {
 	if ctx.IsSet("header-proxy") {
 		f.enabled = ctx.Bool("header-proxy")
 	}
 	return
 }
 
-func (f *Feature) ModifyRequest(w http.ResponseWriter, r *http.Request) {
-	if f.enabled {
-		if ip, err := net.GetIpFromRequest(r); err == nil {
-			if ip != r.RemoteAddr {
-				log.TraceF("setting RemoteAddr to %v (was: %v)", ip, r.RemoteAddr)
-				r.RemoteAddr = ip
-			}
-		} else {
-			log.ErrorF("error getting ip from request: %v", err)
+func (f *CFeature) Use(s feature.System) feature.MiddlewareFn {
+	return func(next http.Handler) (this http.Handler) {
+		if f.enabled {
+			return middleware.RealIP(next)
 		}
+		return http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
+			return
+		})
 	}
 }
