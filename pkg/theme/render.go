28520cce6fe612037a99ee1ffb7f74ff172c34b7
Kevin C. Krinke <kevin@krinke.ca>
2023-03-18 15:12:52 -0400

    pkg/theme (render): remove gtm-specific code

--- pkg/theme/render.go
+++ pkg/theme/render.go
@@ -270,20 +270,6 @@ func (t *Theme) RenderPage(ctx context.Context, p *page.Page) (data []byte, redi
 		}
 	}
 
-	if gtmNonce := ctx.String("GoogleTagManagerHeadScriptNonce", ""); gtmNonce != "" {
-		ctx.SetSpecific("GoogleTagManagerHeadScript", `<!-- Google Tag Manager -->
-<script nonce="`+gtmNonce+`">(function(w,d,s,l,i){w[l]=w[l]||[];w[l].push({'gtm.start':
-new Date().getTime(),event:'gtm.js'});var f=d.getElementsByTagName(s)[0],
-j=d.createElement(s),dl=l!='dataLayer'?'&l='+l:'';j.async=true;j.src=
-'https://www.googletagmanager.com/gtm.js?id='+i+dl;f.parentNode.insertBefore(j,f);
-})(window,document,'script','dataLayer','`+t.Config.GoogleAnalytics.GTM+`');</script>
-<!-- End Google Tag Manager -->`)
-		ctx.SetSpecific("GoogleTagManagerNoScript", `<!-- Google Tag Manager (noscript) -->
-<noscript><iframe src="https://www.googletagmanager.com/ns.html?id=`+t.Config.GoogleAnalytics.GTM+`"
-height="0" width="0" style="display:none;visibility:hidden"></iframe></noscript>
-<!-- End Google Tag Manager (noscript) -->`)
-	}
-
 	data, err = t.Render("single", ctx)
 	return
 }
