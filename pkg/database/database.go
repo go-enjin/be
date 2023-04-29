2dad290f20449eda3e63c068ff6e22b16427c0dd
Kevin C. Krinke <kevin@krinke.ca>
2023-04-29 15:34:49 -0400

    pkg/database: refactoring new db feature pattern; all prior functions are deprecated and to be removed when refactoring complete

--- pkg/database/database.go
+++ pkg/database/database.go
@@ -1,4 +1,4 @@
-// Copyright (c) 2022  The Go-Enjin Authors
+// Copyright (c) 2023  The Go-Enjin Authors
 //
 // Licensed under the Apache License, Version 2.0 (the "License");
 // you may not use this file except in compliance with the License.
@@ -15,85 +15,42 @@
 package database
 
 import (
-	"fmt"
-	"time"
+	"net/http"
 
-	"gorm.io/driver/mysql"
-	"gorm.io/driver/postgres"
-	"gorm.io/driver/sqlite"
-	"gorm.io/gorm"
-	"gorm.io/gorm/logger"
+	"github.com/iancoleman/strcase"
 
-	"github.com/go-enjin/be/pkg/log"
+	"github.com/go-enjin/be/pkg/context"
+	"github.com/go-enjin/be/pkg/feature"
 )
 
-var (
-	Instance *gorm.DB = nil
-)
+const RequestDefaultDbKey context.RequestKey = "DefaultDb"
 
-var (
-	ErrorMissingInstance = fmt.Errorf("missing database instance")
-)
-
-// New returns a newly opened database connection with inherited log settings
-func New(dbType, dsn string) (db *gorm.DB, err error) {
-	config := logger.Config{
-		SlowThreshold:             time.Second,   // Slow SQL threshold
-		LogLevel:                  logger.Silent, // Log level
-		IgnoreRecordNotFoundError: true,          // Ignore ErrRecordNotFound
-		Colorful:                  false,         // Disable color
-	}
-
-	return NewWithLoggerConfig(dbType, dsn, config)
-}
-
-// NewWithLoggerConfig returns a newly opened database connection using the
-// given logger.Config
-func NewWithLoggerConfig(dbType, dsn string, config logger.Config) (db *gorm.DB, err error) {
-
-	cfg := &gorm.Config{
-		Logger: logger.New(
-			log.Logger(), // io writer
-			config,
-		),
-	}
-
-	switch dbType {
-	case "sqlite", "sqlite3":
-		db, err = gorm.Open(sqlite.Open(dsn), cfg)
-	case "mysql":
-		db, err = gorm.Open(mysql.Open(dsn), cfg)
-	case "postgres":
-		db, err = gorm.Open(postgres.Open(dsn), cfg)
-	default:
-		err = fmt.Errorf("unsupported database: %v", dbType)
-	}
+func GetRequestDefaultDB(r *http.Request) (db interface{}) {
+	db = r.Context().Value(RequestDefaultDbKey)
 	return
 }
 
-// Connect opens a new database connection and stores it in a package instance
-// for further use with the Get and MustGet functions
-func Connect(dbType, dsn string) (err error) {
-	Instance, err = New(dbType, dsn)
+func GetRequestDB(tag string, r *http.Request) (db interface{}) {
+	key := MakeRequestDbKey(tag)
+	db = r.Context().Value(key)
 	return
 }
 
-// Get returns the package instance connection or a "missing database
-// instance" error
-func Get() (db *gorm.DB, err error) {
-	if Instance == nil {
-		err = ErrorMissingInstance
-	} else {
-		db = Instance
-	}
+func GetRequestSpecificDB(fTag feature.Tag, tag string, r *http.Request) (db interface{}) {
+	_, spec := MakeRequestDbSpecificKeys(fTag, tag)
+	db = r.Context().Value(spec)
 	return
 }
 
-// MustGet returns the package instance connection or panics with a log.FatalF call
-func MustGet() (db *gorm.DB) {
-	var err error
-	if db, err = Get(); err != nil {
-		log.FatalF("%v", err)
-	}
-	return db
+func MakeRequestDbKey(tag string) (key context.RequestKey) {
+	key = context.RequestKey(strcase.ToCamel(tag))
+	return
+}
+
+func MakeRequestDbSpecificKeys(fTag feature.Tag, tag string) (tagKey, specificKey context.RequestKey) {
+	fKebab := strcase.ToKebab(fTag.String())
+	kebab := strcase.ToKebab(tag)
+	tagKey = context.RequestKey(strcase.ToCamel(tag))
+	specificKey = context.RequestKey(strcase.ToCamel(fKebab + "-" + kebab))
+	return
 }
