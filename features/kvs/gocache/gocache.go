1b2bddef9bcc642d64db5ee2d35231753082f668
Kevin C. Krinke <kevin@krinke.ca>
2023-05-11 15:39:21 -0400

    features/kvs/gocache: initial implementation of new gocache kvs system

---
+++ features/kvs/gocache/gocache.go
@@ -0,0 +1,99 @@
+//go:build kvs_gocache || all
+
+// Copyright (c) 2023  The Go-Enjin Authors
+//
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+//     http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+
+package gocache
+
+import (
+	"fmt"
+
+	"github.com/urfave/cli/v2"
+
+	"github.com/go-enjin/be/pkg/feature"
+	"github.com/go-enjin/be/pkg/kvs"
+)
+
+const Tag feature.Tag = "kvs-gocache"
+
+var (
+	BucketNotFound = fmt.Errorf("bucket not found")
+	BucketExists   = fmt.Errorf("bucket exists")
+)
+
+var (
+	_ Feature     = (*CFeature)(nil)
+	_ MakeFeature = (*CFeature)(nil)
+)
+
+type Feature interface {
+	feature.Feature
+	kvs.KeyValueCaches
+}
+
+type MakeFeature interface {
+	LocalSupport
+
+	Make() Feature
+}
+
+type CFeature struct {
+	feature.CFeature
+
+	caches map[string]kvs.KeyValueCache
+}
+
+func New() MakeFeature {
+	return NewTagged(Tag)
+}
+
+func NewTagged(tag feature.Tag) MakeFeature {
+	f := new(CFeature)
+	f.Init(f)
+	f.FeatureTag = tag
+	return f
+}
+
+func (f *CFeature) Init(this interface{}) {
+	f.CFeature.Init(this)
+	f.caches = make(map[string]kvs.KeyValueCache)
+}
+
+func (f *CFeature) Make() Feature {
+	return f
+}
+
+func (f *CFeature) Build(b feature.Buildable) (err error) {
+	return
+}
+
+func (f *CFeature) Startup(ctx *cli.Context) (err error) {
+	if err = f.CFeature.Startup(ctx); err != nil {
+		return
+	}
+	return
+}
+
+func (f *CFeature) Shutdown() {
+	return
+}
+
+func (f *CFeature) Get(name string) (kvs kvs.KeyValueCache, err error) {
+	if c, ok := f.caches[name]; ok {
+		kvs = c
+		return
+	}
+	err = BucketNotFound
+	return
+}
