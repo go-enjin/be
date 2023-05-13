95e158b123be427c210b3e593e6a224f1bbb9d37
Kevin C. Krinke <kevin@krinke.ca>
2023-05-13 16:53:10 -0400

    pkg/feature: purging old CMiddleware code

--- pkg/feature/middleware.go
+++ pkg/feature/middleware.go
@@ -15,10 +15,7 @@
 package feature
 
 import (
-	"fmt"
 	"net/http"
-
-	"github.com/go-enjin/be/pkg/log"
 )
 
 type MiddlewareFn = func(next http.Handler) (this http.Handler)
@@ -43,35 +40,4 @@ type ApplyMiddleware interface {
 
 type ServePathFeature interface {
 	ServePath(path string, s System, w http.ResponseWriter, r *http.Request) (err error)
-}
-
-var _ Middleware = (*CMiddleware)(nil)
-
-type CMiddleware struct {
-	CFeature
-}
-
-func (f *CMiddleware) Apply(s System) (err error) {
-	return
-}
-
-func (f *CMiddleware) Use(s System) MiddlewareFn {
-	log.DebugF("using %v middleware", f.Self().Tag())
-	return f.Middleware
-}
-
-func (f *CMiddleware) Middleware(next http.Handler) (this http.Handler) {
-	return http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
-		if err := f.Serve(w, r); err != nil {
-			next.ServeHTTP(w, r)
-		}
-	})
-}
-
-func (f *CMiddleware) Serve(w http.ResponseWriter, r *http.Request) (err error) {
-	return fmt.Errorf("feature.Middleware - Serve() not implemented")
-}
-
-func (f *CMiddleware) ServePath(path string, s System, w http.ResponseWriter, r *http.Request) (err error) {
-	return fmt.Errorf("feature.Middleware - ServePath() not implemented")
 }
