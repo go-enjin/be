59cbafa2fc78e3cf5851ea9dc3f794994d0fb0f8
Kevin C. Krinke <kevin@krinke.ca>
2023-05-20 21:18:40 -0400

    pkg/types/theme-types (theme): added Render{Text,Html}TemplateContent methods; added docstrings

--- pkg/types/theme-types/theme.go
+++ pkg/types/theme-types/theme.go
@@ -23,11 +23,24 @@ import (
 )
 
 type Theme interface {
+	// FS returns the theme filesystem
 	FS() fs.FileSystem
+	// GetParentTheme returns a parent Theme instance
 	GetParentTheme() (parent Theme)
+	// GetBlockThemeNames returns a list of names of BlockThemes from theme.toml
 	GetBlockThemeNames() (names []string)
+
+	// NewTextTemplateWithContext constructs a new text/template with the given context
 	NewTextTemplateWithContext(name string, ctx context.Context) (tmpl *textTemplate.Template, err error)
+	// NewTextFuncMapWithContext constructs a new text/template.FuncMap with the given context
 	NewTextFuncMapWithContext(ctx context.Context) (fm textTemplate.FuncMap)
+	// RenderTextTemplateContent renders the given text/template content with the given context
+	RenderTextTemplateContent(ctx context.Context, tmplContent string) (rendered string, err error)
+
+	// NewHtmlTemplateWithContext constructs a new text/template with the given context
 	NewHtmlTemplateWithContext(name string, ctx context.Context) (tmpl *htmlTemplate.Template, err error)
+	// NewHtmlFuncMapWithContext constructs a new text/template.FuncMap with the given context
 	NewHtmlFuncMapWithContext(ctx context.Context) (fm htmlTemplate.FuncMap)
+	// RenderHtmlTemplateContent renders the given text/template content with the given context
+	RenderHtmlTemplateContent(ctx context.Context, tmplContent string) (rendered string, err error)
 }
