67ebfd886826141f5d1bf7a3728a39d6b7991480
Kevin C. Krinke <kevin@krinke.ca>
2023-07-06 14:34:08 -0400

    pkg/maths: code readability cosmetic changes

--- pkg/maths/sorting.go
+++ pkg/maths/sorting.go
@@ -17,7 +17,9 @@ package maths
 import "sort"
 
 type Number interface {
-	int | int8 | int16 | int32 | int64 | uint | uint8 | uint16 | uint32 | uint64 | float32 | float64
+	uint | uint8 | uint16 | uint32 | uint64 |
+		int | int8 | int16 | int32 | int64 |
+		float32 | float64
 }
 
 func SortedNumbers[K Number, V interface{}](data map[K]V) (keys []K) {
