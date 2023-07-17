f322d491a7a70d1bdd442105c75f8924c67d3740
Kevin C. Krinke <kevin@krinke.ca>
2023-07-17 13:14:54 -0400

    pkg/fs (PageStub): implement {Unm,M}arshalBinary methods for gob encoding/decoding support

--- pkg/fs/page-stub.go
+++ pkg/fs/page-stub.go
@@ -16,6 +16,9 @@ package fs
 
 import (
 	"encoding/gob"
+	"encoding/json"
+	"fmt"
+	"strings"
 
 	"github.com/go-enjin/golang-org-x-text/language"
 
@@ -30,6 +33,9 @@ type ValueStubPair struct {
 	Value interface{}
 	Stub  *PageStub
 }
+
+// TODO: serialization of PageStub for BigCache requires FS to be included, implement a means of looking up filesystems
+//       so as to only cache the lookup key and if enjin lacks fs, that's some sort of error, perhaps fatal
 
 type PageStub struct {
 	Origin   string
@@ -52,6 +58,72 @@ func NewPageStub(origin string, enjin beContext.Context, bfs FileSystem, point, 
 		Language: fallback,
 		Fallback: fallback,
 		EnjinCtx: enjin,
+	}
+	return
+}
+
+func (ps *PageStub) MarshalBinary() (data []byte, err error) {
+	return ps.GobEncode()
+}
+
+func (ps *PageStub) UnmarshalBinary(data []byte) (err error) {
+	return ps.GobDecode(data)
+}
+
+func (ps *PageStub) GobEncode() (data []byte, err error) {
+	var ctx []byte
+	if ctx, err = ps.EnjinCtx.AsJSON(); err != nil {
+		return
+	}
+	parts := []string{
+		ps.Origin,
+		ps.FS.ID(),
+		ps.Point,
+		ps.Shasum,
+		ps.Source,
+		ps.Language.String(),
+		ps.Fallback.String(),
+		string(ctx),
+	}
+	text := strings.Join(parts, "\n")
+	data = []byte(text)
+	return
+}
+
+func (ps *PageStub) GobDecode(data []byte) (err error) {
+	text := string(data)
+	parts := strings.Split(text, "\n")
+	if len(parts) != 8 {
+		err = fmt.Errorf("invalid number of data parts: %#+v", parts)
+		return
+	}
+	ps.Origin = parts[0]
+	id := parts[1]
+	if v, ok := GetFileSystem(id); ok {
+		ps.FS = v
+	} else {
+		err = fmt.Errorf("filesystem not found: %v", id)
+		return
+	}
+	ps.Point = parts[2]
+	ps.Shasum = parts[3]
+	ps.Source = parts[4]
+	var lt language.Tag
+	if lt, err = language.Parse(parts[5]); err != nil {
+		err = fmt.Errorf("error parsing filesystem language tag: %v - %v", parts[5], err)
+		return
+	}
+	ps.Language = lt
+	var ft language.Tag
+	if ft, err = language.Parse(parts[6]); err != nil {
+		err = fmt.Errorf("error parsing filesystem language tag: %v - %v", parts[6], err)
+		return
+	}
+	ps.Fallback = ft
+	ps.EnjinCtx = beContext.Context{}
+	if err = json.Unmarshal([]byte(parts[7]), &ps.EnjinCtx); err != nil {
+		err = fmt.Errorf("error parsing filesystem context: %v", err)
+		return
 	}
 	return
 }
