3f034d2bad90483aae059d9ddbb7ca0cef508442
Kevin C. Krinke <kevin@krinke.ca>
2023-08-10 14:31:03 -0400

    features/pages/formats/bb: new github.com/frustra/bbcode based page format feature

---
+++ features/pages/formats/bb/bb.go
@@ -0,0 +1,238 @@
+// Copyright (c) 2022  The Go-Enjin Authors
+//
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+//     http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+
+package bb
+
+import (
+	"fmt"
+	"html/template"
+	"net/http"
+	"strings"
+
+	"github.com/blevesearch/bleve/v2/mapping"
+	"github.com/frustra/bbcode"
+	"golang.org/x/net/html"
+
+	"github.com/go-enjin/golang-org-x-text/language"
+
+	"github.com/go-enjin/be/pkg/context"
+	"github.com/go-enjin/be/pkg/feature"
+	"github.com/go-enjin/be/pkg/lang"
+	"github.com/go-enjin/be/pkg/log"
+	"github.com/go-enjin/be/pkg/page"
+	beStrings "github.com/go-enjin/be/pkg/strings"
+	"github.com/go-enjin/be/pkg/types/theme-types"
+)
+
+const Tag feature.Tag = "pages-formats-bb"
+
+var (
+	_ Feature     = (*CFeature)(nil)
+	_ MakeFeature = (*CFeature)(nil)
+)
+
+type Feature interface {
+	feature.Feature
+	types.Format
+}
+
+type MakeFeature interface {
+	Make() Feature
+}
+
+type CFeature struct {
+	feature.CFeature
+
+	compiler bbcode.Compiler
+}
+
+func New() MakeFeature {
+	f := new(CFeature)
+	f.Init(f)
+	f.FeatureTag = Tag
+	return f
+}
+
+func (f *CFeature) Init(this interface{}) {
+	f.CFeature.Init(this)
+	f.compiler = bbcode.NewCompiler(false, false)
+}
+
+func (f *CFeature) AutoCloseTags(enabled bool) MakeFeature {
+	f.compiler.AutoCloseTags = enabled
+	return f
+}
+
+func (f *CFeature) IgnoreUnmatchedClosingTags(ignored bool) MakeFeature {
+	f.compiler.IgnoreUnmatchedClosingTags = ignored
+	return f
+}
+
+func (f *CFeature) SortOutputAttributes(sorted bool) MakeFeature {
+	f.compiler.SortOutputAttributes = sorted
+	return f
+}
+
+func (f *CFeature) AddTag(name string, fn bbcode.TagCompilerFunc) MakeFeature {
+	f.compiler.SetTag(name, fn)
+	return f
+}
+
+func (f *CFeature) RemoveTag(name string) MakeFeature {
+	f.compiler.SetTag(name, nil)
+	return f
+}
+
+func (f *CFeature) Make() Feature {
+	return f
+}
+
+func (f *CFeature) Name() (name string) {
+	name = "bb"
+	return
+}
+
+func (f *CFeature) Extensions() (extensions []string) {
+	extensions = append(extensions, "bb", "bb.tmpl")
+	return
+}
+
+func (f *CFeature) Label() (label string) {
+	label = "Bulletin-Board"
+	return
+}
+
+func (f *CFeature) Prepare(ctx context.Context, content string) (out context.Context, err error) {
+	return
+}
+
+func (f *CFeature) Process(ctx context.Context, t types.Theme, content string) (html template.HTML, redirect string, err *types.EnjinError) {
+	html = template.HTML(f.Enjin.TranslateShortcodes(f.compiler.Compile(content), ctx))
+	return
+}
+
+func (f *CFeature) SearchDocumentMapping(tag language.Tag) (doctype string, dm *mapping.DocumentMapping) {
+	doctype, _, dm = f.NewDocumentMapping(tag)
+	return
+}
+
+func (f *CFeature) AddSearchDocumentMapping(tag language.Tag, indexMapping *mapping.IndexMappingImpl) {
+	doctype, _, dm := f.NewDocumentMapping(tag)
+	indexMapping.AddDocumentMapping(doctype, dm)
+}
+func (f *CFeature) IndexDocument(thing interface{}) (out interface{}, err error) {
+	pg, _ := thing.(*page.Page) // FIXME: this "thing" avoids package import loops
+
+	t := f.Enjin.MustGetTheme()
+	r, _ := http.NewRequest("GET", pg.Url, nil)
+	r = lang.SetTag(r, pg.LanguageTag)
+	for _, ptp := range feature.FilterTyped[feature.PageTypeProcessor](f.Enjin.Features().List()) {
+		if v, _, processed, e := ptp.ProcessRequestPageType(r, pg); e != nil {
+			log.ErrorF("error processing page type for html format indexing: %v - %v", pg.Url, e)
+		} else if processed {
+			pg = v
+		}
+	}
+
+	var rendered string
+	if strings.HasSuffix(pg.Format, ".tmpl") {
+		if rendered, err = t.RenderHtmlTemplateContent(pg.Context, pg.Content); err != nil {
+			err = fmt.Errorf("error rendering .html.tmpl content: %v", err)
+			return
+		}
+	} else {
+		rendered = pg.Content
+	}
+
+	doc := NewBulletinBoardDocument(pg.Language, pg.Url, pg.Title)
+	var parsed *html.Node
+	if parsed, err = html.Parse(strings.NewReader(rendered)); err != nil {
+		return
+	}
+
+	skipNext := false
+	addLinkNext := false
+	addHeadingNext := false
+
+	contents := ""
+
+	var walk func(node *html.Node)
+	walk = func(node *html.Node) {
+
+		if node.Type == html.ElementNode {
+			switch node.Data {
+			case "a":
+				addLinkNext = true
+			case "h1", "h2", "h3", "h4", "h5", "h6":
+				addHeadingNext = true
+			case "div":
+				for _, attr := range node.Attr {
+					if attr.Key == "class" {
+						switch attr.Val {
+						case "h1", "h2", "h3", "h4", "h5", "h6":
+							addHeadingNext = true
+							break
+						}
+					}
+					if addHeadingNext {
+						break
+					}
+				}
+			case "style":
+				skipNext = true
+			}
+		} else if node.Type == html.TextNode {
+			if skipNext {
+				skipNext = false
+				// log.DebugF("skipping text: %v - %v", node.Type, node.Data)
+			} else {
+				data := beStrings.StripTmplTags(node.Data)
+				data = strings.ReplaceAll(data, "permalink", "")
+				data = strings.ReplaceAll(data, "top", "")
+				if !beStrings.Empty(data) {
+					if addLinkNext {
+						addLinkNext = false
+						// log.DebugF("adding html link: %v", data)
+						doc.AddLink(data)
+						contents = beStrings.AppendWithSpace(contents, data)
+					} else if addHeadingNext {
+						addHeadingNext = false
+						// log.DebugF("adding html heading: %v", data)
+						doc.AddHeading(data)
+					} else {
+						contents = beStrings.AppendWithSpace(contents, data)
+					}
+				} else {
+					addLinkNext = false
+					addHeadingNext = false
+				}
+			}
+		}
+
+		for c := node.FirstChild; c != nil; c = c.NextSibling {
+			walk(c)
+		}
+	}
+
+	walk(parsed)
+
+	if !beStrings.Empty(contents) {
+		doc.AddContent(contents)
+		// log.DebugF("adding html contents:\n%v", contents)
+	}
+
+	out = doc
+	err = nil
+	return
+}
---
+++ features/pages/formats/bb/search.go
@@ -0,0 +1,67 @@
+// Copyright (c) 2022  The Go-Enjin Authors
+//
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+//     http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+
+package bb
+
+import (
+	"github.com/blevesearch/bleve/v2/mapping"
+
+	"github.com/go-enjin/golang-org-x-text/language"
+
+	"github.com/go-enjin/be/pkg/search"
+)
+
+var _ Document = (*CDocument)(nil)
+
+type Document interface {
+	search.Document
+	AddLink(text string)
+	AddHeading(text string)
+}
+
+type CDocument struct {
+	search.CDocument
+
+	Links    []string `json:"links"`
+	Headings []string `json:"headings"`
+}
+
+func NewBulletinBoardDocument(language, url, title string) (doc *CDocument) {
+	doc = new(CDocument)
+	doc.Type = "bb"
+	doc.Url = url
+	doc.Title = title
+	doc.Language = language
+	return
+}
+
+func (d *CDocument) Self() interface{} {
+	return d
+}
+
+func (d *CDocument) AddLink(text string) {
+	d.Links = append(d.Links, text)
+}
+
+func (d *CDocument) AddHeading(text string) {
+	d.Headings = append(d.Headings, text)
+}
+
+func (f *CFeature) NewDocumentMapping(tag language.Tag) (doctype, analyzer string, dm *mapping.DocumentMapping) {
+	doctype = f.Name()
+	analyzer, dm = search.NewDocumentMapping(tag)
+	dm.AddFieldMappingsAt("links", search.NewDefaultTextFieldMapping(analyzer))
+	dm.AddFieldMappingsAt("headings", search.NewDefaultTextFieldMapping(analyzer))
+	return
+}
