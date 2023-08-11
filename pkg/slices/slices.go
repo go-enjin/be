edbd16b7d3eb4ef50773b18b1d5f1e2b8b89b3c2
Kevin C. Krinke <kevin@krinke.ca>
2023-08-11 14:22:46 -0400

    pkg/slices: added Prune, Remove, Push, Pop, Shift and Unshift generic slice helpers; code cosmetic changes

--- pkg/slices/slices.go
+++ pkg/slices/slices.go
@@ -15,26 +15,83 @@
 package slices
 
 // Copy creates a new slice (array) from the given slice
-func Copy[T interface{}, S ~[]T](slice S) (copied S) {
+func Copy[V interface{}, S ~[]V](slice S) (copied S) {
 	copied = make(S, len(slice))
 	copy(copied, slice)
 	return
 }
 
 // Truncate creates a new slice (array), of specified length, from the given slice
-func Truncate[T interface{}, S ~[]T](slice S, length int) (truncated S) {
+func Truncate[V interface{}, S ~[]V](slice S, length int) (truncated S) {
 	truncated = make(S, length)
 	copy(truncated, slice)
 	return
 }
 
 // Insert creates a new slice (array) from the given slice, with additional values inserted at the given index
-func Insert[T interface{}, S ~[]T](slice S, at int, values ...T) (modified S) {
+func Insert[V interface{}, S ~[]V](slice S, at int, values ...V) (modified S) {
 	before := slice[:at]
 	after := slice[at:]
 	modified = make(S, 0)
 	modified = append(modified, before...)
 	modified = append(modified, values...)
 	modified = append(modified, after...)
+	return
+}
+
+// Prune removes all instances of the specified value from a copy of the given slice
+func Prune[V comparable, S ~[]V](slice S, value V) (modified S, rmIndexes []int) {
+	modified = make(S, 0)
+	for i, v := range slice {
+		if v == value {
+			rmIndexes = append(rmIndexes, i)
+		} else {
+			modified = append(modified, v)
+		}
+	}
+	return
+}
+
+// Remove creates a new slice (array) from the given slice, with the specified index removed
+func Remove[V interface{}, S ~[]V](slice S, at int) (modified S) {
+	modified = make(S, 0)
+	if at >= 0 && at < len(slice) {
+		modified = append(modified, slice[:at]...)
+		modified = append(modified, slice[at+1:]...)
+	} else {
+		modified = append(modified, slice...)
+	}
+	return
+}
+
+// Push appends the given value to a new copy of the given slice
+func Push[V interface{}, S ~[]V](slice S, values ...V) (modified S) {
+	modified = append(Copy(slice), values...)
+	return
+}
+
+// Pop removes the last value from a Copy of the slice and returns it
+func Pop[V interface{}, S ~[]V](slice S) (modified S, value V) {
+	if last := len(slice) - 1; last > -1 {
+		value = slice[last]
+		modified = Truncate(slice, last)
+	}
+	return
+}
+
+// Shift prepends the given value to a new copy of the given slice
+func Shift[V interface{}, S ~[]V](slice S, values ...V) (modified S) {
+	modified = make(S, 0)
+	modified = append(modified, values...)
+	modified = append(modified, slice...)
+	return
+}
+
+// Unshift removes the first value from a Copy of the slice and returns it
+func Unshift[V interface{}, S ~[]V](slice S) (modified S, value V) {
+	if len(slice) > 0 {
+		value = slice[0]
+		modified = Copy(slice[1:])
+	}
 	return
 }
