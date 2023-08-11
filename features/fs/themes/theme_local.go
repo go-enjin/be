3240f0107b900220a4e922fb1d719f7e1c6c9910
Kevin C. Krinke <kevin@krinke.ca>
2023-08-11 14:21:02 -0400

    features/fs/themes (local): refactored to use new theme.New constructor, migrating from theme.NewLocal

--- features/fs/themes/theme_local.go
+++ features/fs/themes/theme_local.go
@@ -17,6 +17,10 @@
 package themes
 
 import (
+	"fmt"
+
+	"github.com/go-enjin/be/drivers/fs/local"
+	"github.com/go-enjin/be/pkg/fs"
 	"github.com/go-enjin/be/pkg/log"
 	bePath "github.com/go-enjin/be/pkg/path"
 	"github.com/go-enjin/be/pkg/theme"
@@ -32,14 +36,40 @@ type ThemeLocalSupport interface {
 	LocalThemes(path string) MakeFeature
 }
 
+func (f *CFeature) loadLocalTheme(path string) (t *theme.Theme, err error) {
+	if !bePath.IsDir(path) {
+		err = fmt.Errorf("directory not found: %v", path)
+		return
+	}
+	var themeFs, staticFs fs.FileSystem
+	if themeFs, err = local.New(f.Tag().String(), path); err != nil {
+		err = fmt.Errorf("error mounting local filesystem: %v - %v", path, err)
+		return
+	}
+	if staticFs, err = local.New(f.Tag().String(), path+"/static"); err == nil {
+		fs.RegisterFileSystem("/", staticFs)
+	} else {
+		staticFs = nil
+	}
+
+	if t, err = theme.New(f.Tag().String(), path, themeFs, staticFs); err != nil {
+		err = fmt.Errorf("error loading theme: %v - %v", path, err)
+		return
+	}
+
+	return
+}
+
 func (f *CFeature) LocalTheme(path string) MakeFeature {
 	var err error
 	var t *theme.Theme
-	if t, err = theme.NewLocal(f.Tag().String(), path); err != nil {
-		log.FatalF("error loading local theme: %v", err)
-	} else {
-		log.DebugF("loaded local theme: %v", t.Name)
+
+	if t, err = f.loadLocalTheme(path); err != nil {
+		log.FatalDF(1, "%v", err)
 	}
+
+	log.DebugF("loaded local theme: %v", t.Name)
+
 	f.AddTheme(t)
 	return f
 }
@@ -52,12 +82,11 @@ func (f *CFeature) LocalThemes(path string) MakeFeature {
 		return nil
 	}
 	for _, p := range paths {
-		name := bePath.Base(p)
-		log.DebugF("loading theme: %v", p)
-		if f.themes[name], err = theme.NewLocal(f.Tag().String(), p); err != nil {
-			delete(f.themes, name)
-			log.FatalF("error loading theme: %v", err)
-			return nil
+		if t, e := f.loadLocalTheme(p); e != nil {
+			log.FatalDF(1, "%s", err)
+		} else {
+			f.themes[t.Name] = t
+			log.DebugF("loaded local theme: %v", t.Name)
 		}
 	}
 	return f
