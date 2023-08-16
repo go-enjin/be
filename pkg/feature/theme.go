d435cc71619917624e19d20fd1e4ccedddf9de3a
Kevin C. Krinke <kevin@krinke.ca>
2023-08-16 13:31:09 -0400

    pkg/feature (theme): new abstracted Theme interface replacing pkg/theme

---
+++ pkg/feature/theme.go
@@ -0,0 +1,120 @@
+// Copyright (c) 2023  The Go-Enjin Authors
+//
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+//     http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+
+package feature
+
+import (
+	htmlTemplate "html/template"
+	"net/http"
+	textTemplate "text/template"
+
+	"github.com/go-enjin/be/pkg/context"
+	"github.com/go-enjin/be/pkg/fs"
+	"github.com/go-enjin/be/pkg/net/headers/policy/csp"
+	"github.com/go-enjin/be/pkg/net/headers/policy/permissions"
+	"github.com/go-enjin/be/pkg/page"
+)
+
+type Author struct {
+	Name     string
+	Homepage string
+}
+
+type ThemeConfig struct {
+	Name        string
+	Parent      string
+	License     string
+	LicenseLink string
+	Description string
+	Homepage    string
+	Authors     []Author
+	Extends     string
+
+	RootStyles  []htmlTemplate.CSS
+	BlockStyles map[string][]htmlTemplate.CSS
+	BlockThemes map[string]map[string]interface{}
+
+	FontawesomeLinks   map[string]string
+	FontawesomeClasses []string
+
+	CacheControl string
+
+	PermissionsPolicy     []permissions.Directive
+	ContentSecurityPolicy csp.ContentSecurityPolicyConfig
+
+	Context context.Context
+}
+
+type Theme interface {
+	Name() (name string)
+	Reload() (err error)
+
+	GetParent() (parent Theme)
+	GetBlockThemeNames() (names []string)
+
+	ThemeFS() fs.FileSystem
+	StaticFS() fs.FileSystem
+	Locales() (locales fs.FileSystem, ok bool)
+
+	GetConfig() (config ThemeConfig)
+
+	Layouts() ThemeLayouts
+	FindLayout(named string) (layout ThemeLayout, name string, ok bool)
+
+	AddFormatProvider(providers ...PageFormatProvider)
+	PageFormatProvider
+
+	Middleware(next http.Handler) http.Handler
+}
+
+type ThemeLayout interface {
+	Name() (name string)
+
+	NewHtmlTemplate(enjin Internals, ctx context.Context) (tmpl *htmlTemplate.Template, err error)
+	NewTextTemplate(enjin Internals, ctx context.Context) (tmpl *textTemplate.Template, err error)
+
+	NewHtmlTemplateFrom(enjin Internals, parent ThemeLayout, ctx context.Context) (tmpl *htmlTemplate.Template, err error)
+	NewTextTemplateFrom(enjin Internals, parent ThemeLayout, ctx context.Context) (tmpl *textTemplate.Template, err error)
+
+	ApplyHtmlTemplate(enjin Internals, tt *htmlTemplate.Template, ctx context.Context) (err error)
+	ApplyTextTemplate(enjin Internals, tt *textTemplate.Template, ctx context.Context) (err error)
+}
+
+type ThemeLayouts interface {
+	GetLayout(name string) (layout ThemeLayout)
+	SetLayout(name string, layout ThemeLayout)
+
+	NewHtmlTemplate(enjin Internals, name string, ctx context.Context) (tmpl *htmlTemplate.Template, err error)
+	NewTextTemplate(enjin Internals, name string, ctx context.Context) (tmpl *textTemplate.Template, err error)
+}
+
+type ThemeRenderer interface {
+	Feature
+	Render(view string, ctx context.Context) (data []byte, err error)
+	RenderPage(ctx context.Context, p *page.Page) (data []byte, redirect string, err error)
+
+	NewHtmlTemplateWith(name string, ctx context.Context) (tt *htmlTemplate.Template, err error)
+	NewTextTemplateWith(name string, ctx context.Context) (tt *textTemplate.Template, err error)
+
+	RenderHtmlTemplateContent(ctx context.Context, tmplContent string) (rendered string, err error)
+	RenderTextTemplateContent(ctx context.Context, tmplContent string) (rendered string, err error)
+
+	NewHtmlTemplateFromContext(view string, ctx context.Context) (tt *htmlTemplate.Template, err error)
+	NewTextTemplateFromContext(view string, ctx context.Context) (tt *textTemplate.Template, err error)
+}
+
+type RoutePagesHandler interface {
+	Feature
+	RoutePage(w http.ResponseWriter, r *http.Request)
+}
