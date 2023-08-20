146e0a9e1bd26e5c71adea3c346202971d3e8f87
Kevin C. Krinke <kevin@krinke.ca>
2023-08-20 15:16:02 -0400

    presets/{defaults,essentials}: two new features, classified as 'presets', which simply add other features during the Build phase; essentials is a basic enjin serving tmpl+html format pages; defaults are all of essentials, with all formats and includes permalink, query, htmlify and proxy; all added features use default tags

---
+++ presets/defaults/defaults.go
@@ -0,0 +1,98 @@
+// Copyright (c) 2023  The Go-Enjin Authors
+//
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+//     http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+
+package defaults
+
+import (
+	"github.com/urfave/cli/v2"
+
+	"github.com/go-enjin/be/drivers/theme/renderer"
+	"github.com/go-enjin/be/features/outputs/htmlify"
+	"github.com/go-enjin/be/features/pages/formats"
+	"github.com/go-enjin/be/features/pages/funcmaps"
+	"github.com/go-enjin/be/features/pages/partials"
+	"github.com/go-enjin/be/features/pages/permalink"
+	"github.com/go-enjin/be/features/pages/query"
+	"github.com/go-enjin/be/features/requests/headers/proxy"
+	"github.com/go-enjin/be/features/srv/listeners/httpd"
+	"github.com/go-enjin/be/features/srv/pages"
+	"github.com/go-enjin/be/pkg/feature"
+)
+
+var (
+	_ Feature     = (*CFeature)(nil)
+	_ MakeFeature = (*CFeature)(nil)
+)
+
+const Tag feature.Tag = "preset-defaults"
+
+type Feature interface {
+	feature.Feature
+}
+
+type MakeFeature interface {
+	Make() Feature
+}
+
+type CFeature struct {
+	feature.CFeature
+}
+
+func New() MakeFeature {
+	return NewTagged(Tag)
+}
+
+func NewTagged(tag feature.Tag) MakeFeature {
+	f := new(CFeature)
+	f.Init(f)
+	f.FeatureTag = tag
+	return f
+}
+
+func (f *CFeature) Init(this interface{}) {
+	f.CFeature.Init(this)
+	return
+}
+
+func (f *CFeature) Make() (feat Feature) {
+	return f
+}
+
+func (f *CFeature) Build(b feature.Buildable) (err error) {
+	if err = f.CFeature.Build(b); err != nil {
+		return
+	}
+	b.AddFeature(formats.New().Defaults().Make())
+	b.AddFeature(funcmaps.New().Defaults().Make())
+	b.AddFeature(partials.New().Make())
+	b.AddFeature(permalink.New().Make())
+	b.AddFeature(query.New().Make())
+	b.AddFeature(renderer.New().Make())
+	b.AddFeature(htmlify.New().Make())
+	b.AddFeature(proxy.New().Enable().Make())
+	b.AddFeature(pages.New().Make())
+	b.AddFeature(httpd.New().Make())
+	return
+}
+
+func (f *CFeature) Startup(ctx *cli.Context) (err error) {
+	if err = f.CFeature.Startup(ctx); err != nil {
+		return
+	}
+	return
+}
+
+func (f *CFeature) Shutdown() {
+	f.CFeature.Shutdown()
+}
