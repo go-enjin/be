93432b8ce0ac6e879aa316651668d3e0813ca344
Kevin C. Krinke <kevin@krinke.ca>
2023-10-03 14:43:53 -0400

    pkg/strings: initial implementation of a simplified unified diff function

---
+++ pkg/strings/diff.go
@@ -0,0 +1,31 @@
+// Copyright (c) 2023  The Go-Enjin Authors
+//
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+//     http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+
+package strings
+
+import (
+	"fmt"
+
+	"github.com/hexops/gotextdiff"
+	"github.com/hexops/gotextdiff/myers"
+	"github.com/hexops/gotextdiff/span"
+)
+
+func Diff(filename, a, b string) (patch string) {
+	patch = fmt.Sprint(gotextdiff.ToUnified(
+		"a/"+filename, "b/"+filename, a,
+		myers.ComputeEdits(span.URIFromPath("a/"+filename), a, b),
+	))
+	return
+}
