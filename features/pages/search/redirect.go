1edecb77c37fc68a7742a0ed2692f87370bb8ef5
Kevin C. Krinke <kevin@krinke.ca>
2023-10-05 15:11:36 -0400

    features/pages/search: use forms.StrictSanitize

--- features/pages/search/redirect.go
+++ features/pages/search/redirect.go
@@ -37,7 +37,7 @@ func (f *CFeature) handleQueryRedirect(r *http.Request) (redirect string, err er
 	for k, v := range r.URL.Query() {
 		switch k {
 		case "nonce":
-			value := forms.StrictPolicy(v[0])
+			value := forms.StrictSanitize(v[0])
 			if vv, e := url.QueryUnescape(value); e != nil {
 				log.ErrorRF(r, "error un-escaping url path: %v", e)
 			} else {
@@ -51,7 +51,7 @@ func (f *CFeature) handleQueryRedirect(r *http.Request) (redirect string, err er
 				break
 			}
 		case "query":
-			query = forms.StrictPolicy(v[0])
+			query = forms.StrictSanitize(v[0])
 			if vv, e := url.PathUnescape(query); e != nil {
 				log.ErrorRF(r, "error un-escaping url path: %v", e)
 			} else {
