673198b33c8f0f9626d68eda46528fbc1d81c4e2
Kevin C. Krinke <kevin@krinke.ca>
2023-10-20 13:56:06 -0400

    pkg/maths (constraints): renamed SignedInteger to just Integer; added Integers which is signed+unsigned; added Complex; code cosmetic changes

--- pkg/maths/constraints.go
+++ pkg/maths/constraints.go
@@ -15,17 +15,25 @@
 package maths
 
 type Number interface {
-	UnsignedInteger | SignedInteger | Decimal
+	UnsignedInteger | Integer | Decimal
+}
+
+type Integers interface {
+	Integer | UnsignedInteger
+}
+
+type Integer interface {
+	~int | ~int8 | ~int16 | ~int32 | ~int64
 }
 
 type UnsignedInteger interface {
 	~uint | ~uint8 | ~uint16 | ~uint32 | ~uint64
 }
 
-type SignedInteger interface {
-	~int | ~int8 | ~int16 | ~int32 | ~int64
-}
-
 type Decimal interface {
 	~float32 | ~float64
+}
+
+type Complex interface {
+	~complex64 | ~complex128
 }
