502057aca14a10e1cabc6492ebaef46ee8b6d861
Kevin C. Krinke <kevin@krinke.ca>
2023-10-23 12:53:08 -0400

    pkg/feature (SiteIncluding): new abstract type to consolidate code duplication managing lists of specific feature types

---
+++ pkg/feature/site-including.go
@@ -0,0 +1,73 @@
+// Copyright (c) 2023  The Go-Enjin Authors
+//
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+//     http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+
+package feature
+
+type SiteIncludingMakeFeature[M interface{}] interface {
+	Include(features ...Feature) M
+	Including(tags ...Tag) M
+}
+
+type CSiteIncluding[T interface{}, M interface{}] struct {
+	IncludeFeatures   Features
+	IncludingFeatures Tags
+	Features          TypedFeatures[T]
+
+	_siteIncludingThis interface{}
+}
+
+func (si *CSiteIncluding[T, M]) InitSiteIncluding(this interface{}) {
+	si._siteIncludingThis = this
+	return
+}
+
+func (si *CSiteIncluding[T, M]) Include(features ...Feature) M {
+	si.IncludeFeatures = append(si.IncludeFeatures, features...)
+	t, _ := si._siteIncludingThis.(M)
+	return t
+}
+
+func (si *CSiteIncluding[T, M]) Including(tags ...Tag) M {
+	si.IncludingFeatures = append(si.IncludingFeatures, tags...)
+	t, _ := si._siteIncludingThis.(M)
+	return t
+}
+
+func (si *CSiteIncluding[T, M]) BuildSiteIncluding(b Buildable) {
+	for _, ef := range si.IncludeFeatures {
+		b.AddFeature(ef)
+	}
+	return
+}
+
+func (si *CSiteIncluding[T, M]) StartupSiteIncluding(enjin Internals) {
+
+	for _, v := range si.IncludeFeatures {
+		if ef, ok := v.This().(T); ok {
+			si.Features = append(si.Features, ef)
+		}
+	}
+
+	for _, tag := range si.IncludingFeatures {
+		if !si.Features.Has(tag) {
+			if v, found := enjin.Features().Get(tag); found {
+				if ef, ok := v.This().(T); ok {
+					si.Features = append(si.Features, ef)
+				}
+			}
+		}
+	}
+
+	return
+}
