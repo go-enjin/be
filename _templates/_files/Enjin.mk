7e4eab9dbdc18558fb890ac457fc6afcf9ce1ac6
Kevin C. Krinke <kevin@krinke.ca>
2023-11-24 14:27:57 -0500

    _templates/_files (Enjin.mk): updated to v0.2.13

--- _templates/_files/Enjin.mk
+++ _templates/_files/Enjin.mk
@@ -17,7 +17,7 @@
 #: uncomment to echo instead of execute
 #CMD=echo
 
-ENJIN_MK_VERSION := v0.2.12
+ENJIN_MK_VERSION := v0.2.13
 
 #
 #: phony make targets
@@ -1096,7 +1096,7 @@ dev: run
 ifneq (${DLV_BIN},)
 .PHONY: dlv
 
-dlv: DLV_DEBUG=true
+dlv: export DLV_DEBUG=true
 dlv: dev
 else
 install-dlv: _golang
@@ -1176,6 +1176,9 @@ release-dev-run: release
 
 pre-release-dev-run: pre-release
 	@( $(MAKE) dev 2>&1 ) | perl -p -e 'use Term::ANSIColor qw(colored);while (my $$line = <>) {print STDOUT process_line($$line)."\n";}exit(0);sub process_line {my ($$line) = @_;chomp($$line);if ($$line =~ m!^\[(\d+\-\d+\.\d+)\]\s+([A-Z]+)\s+(.+?)\s*$$!) {my ($$datestamp, $$level, $$message) = ($$1, $$2, $$3);my $$colour = "white";if ($$level eq "ERROR") {$$colour = "bold white on_red";} elsif ($$level eq "INFO") {$$colour = "green";} elsif ($$level eq "DEBUG") {$$colour = "yellow";}my $$out = "[".colored($$datestamp, "blue")."]";$$out .= " ".colored($$level, $$colour);if ($$level eq "DEBUG") {$$out .= "\t";if ($$message =~ m!^(.+?)\:(\d+)\s+\[(.+?)\]\s+(.+?)\s*$$!) {my ($$file, $$ln, $$tag, $$info) = ($$1, $$2, $$3, $$4);$$out .= colored($$file, "bright_blue");$$out .= ":".colored($$ln, "blue");$$out .= " [".colored($$tag, "bright_blue")."]";$$out .= " ".colored($$info, "bold cyan");} else {$$out .= $$message;}} elsif ($$level eq "ERROR") {$$out .= "\t".colored($$message, $$colour);} elsif ($$level eq "INFO") {$$out .= "\t".colored($$message, $$colour);} else {$$out .= "\t".$$message;}return $$out;}return $$line;}'
+
+pre-release-dlv-run: export DLV_DEBUG=true
+pre-release-dlv-run: pre-release-dev-run
 
 stop:
 	@RUNNING_PIDS=$$(\
