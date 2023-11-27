a8794cad087530a138d54dae304c4da2ed92fce3
Kevin C. Krinke <kevin@krinke.ca>
2023-11-27 14:11:15 -0500

    pkg/feature (site-feature): new SiteUserRequestModifier interface

--- pkg/feature/site-feature.go
+++ pkg/feature/site-feature.go
@@ -19,10 +19,11 @@ import (
 
 	"github.com/go-chi/chi/v5"
 
+	"github.com/go-enjin/golang-org-x-text/message"
+
 	beContext "github.com/go-enjin/be/pkg/context"
 	"github.com/go-enjin/be/pkg/feature/signaling"
 	"github.com/go-enjin/be/pkg/menu"
-	"github.com/go-enjin/golang-org-x-text/message"
 )
 
 type SiteFeatures TypedFeatures[SiteFeature]
@@ -65,4 +66,10 @@ type SiteRootFeature interface {
 	SiteFeature
 
 	SiteRootHandler() (this http.Handler)
+}
+
+type SiteUserRequestModifier interface {
+	SiteFeature
+
+	ModifyUserRequest(au AuthUser, r *http.Request) (modified *http.Request)
 }
