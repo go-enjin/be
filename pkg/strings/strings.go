5cfee87fd90bb957c2317b6ac54d7b84a8d702d2
Kevin C. Krinke <kevin@krinke.ca>
2024-02-13 15:07:16 -0500

    pkg/strings: migrated to go-corelibs/htmlcss

--- pkg/strings/strings.go
+++ pkg/strings/strings.go
@@ -15,13 +15,7 @@
 package strings
 
 import (
-	"fmt"
-	"html/template"
-	"strings"
-
-	"github.com/go-corelibs/slices"
-	clStrings "github.com/go-corelibs/strings"
-	"github.com/go-enjin/be/pkg/regexps"
+	"github.com/go-corelibs/htmlcss"
 )
 
 //func TitleCase(input string) (output string) {
@@ -42,97 +36,20 @@ import (
 //	return
 //}
 
-func ParseHtmlTagAttributes(input interface{}) (attributes map[string]interface{}, err error) {
-	attributes = make(map[string]interface{})
-
-	parseAndUpdate := func(raw string) (e error) {
-		parts := regexps.RxSplitHtmlTagAttributes.Split(raw, -1)
-		for _, part := range parts {
-			if regexps.RxParseHtmlTagKeyOnly.MatchString(part) {
-				if m := regexps.RxParseHtmlTagKeyOnly.FindAllStringSubmatch(part, -1); m != nil {
-					key := m[0][1]
-					attributes[key] = nil
-				}
-			} else if regexps.RxParseHtmlTagKeyValue.MatchString(part) {
-				if m := regexps.RxParseHtmlTagKeyValue.FindAllStringSubmatch(part, -1); m != nil {
-					key, quoted := m[0][1], m[0][2]
-					unquoted := clStrings.TrimQuotes(quoted)
-					attributes[key] = unquoted
-				}
-			} else {
-				e = fmt.Errorf(`unsupported HTMLAttr format: %v`, part)
-				return
-			}
-		}
-		return
-	}
-
-	switch v := input.(type) {
-
-	case string:
-		err = parseAndUpdate(v)
-	case template.HTML:
-		err = parseAndUpdate(string(v))
-	case template.HTMLAttr:
-		err = parseAndUpdate(string(v))
-	case []string:
-		for _, tha := range v {
-			if err = parseAndUpdate(tha); err != nil {
-				return
-			}
-		}
-	case []template.HTML:
-		for _, tha := range v {
-			if err = parseAndUpdate(string(tha)); err != nil {
-				return
-			}
-		}
-	case []template.HTMLAttr:
-		for _, tha := range v {
-			if err = parseAndUpdate(string(tha)); err != nil {
-				return
-			}
-		}
-
-	default:
-		err = fmt.Errorf("unknown input type: (%T) %+v", v, v)
-	}
-	return
-}
-
-func UniqueFromSpaceSep(value string, original []string) (updated []string) {
-	updated = original
-	parts := regexps.RxSplitHtmlTagAttributes.Split(value, -1)
-	for _, part := range parts {
-		if !slices.Present(part, updated...) {
-			updated = append(updated, part)
-		}
-	}
-	return
-}
-
 func AddClassNamesToNjnBlock(data map[string]interface{}, classes ...string) map[string]interface{} {
 	if v, ok := data["class"]; ok {
-		var unique []string
 		switch t := v.(type) {
 		case string:
-			parts := regexps.RxSplitHtmlTagAttributes.Split(t, -1)
-			for _, p := range parts {
-				unique = UniqueFromSpaceSep(p, unique)
-			}
-			for _, c := range classes {
-				unique = UniqueFromSpaceSep(c, unique)
-			}
+			data["class"] = htmlcss.AddClassNames(t, classes...)
 		case []interface{}:
+			var list []interface{}
 			for _, iface := range t {
-				if s, ok := iface.(string); ok {
-					unique = UniqueFromSpaceSep(s, unique)
+				if vs, ok := iface.(string); ok {
+					list = append(list, htmlcss.AddClassNames(vs, classes...))
 				}
 			}
+			data["class"] = list
 		}
-		data["class"] = strings.Join(unique, " ")
-	} else {
-		data["class"] = strings.Join(classes, " ")
 	}
 	return data
 }
