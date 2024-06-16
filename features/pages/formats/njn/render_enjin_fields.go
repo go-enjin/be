d4f9f6c263511060cd6e9ec627c493c2a2b14314
Kevin C. Krinke <kevin@krinke.ca>
2024-06-16 13:07:06 -0400

    features/pages/formats/njn: migrate to pkg/pages from old pkg/maps

--- features/pages/formats/njn/render_enjin_fields.go
+++ features/pages/formats/njn/render_enjin_fields.go
@@ -20,12 +20,12 @@ import (
 	"html/template"
 
 	"github.com/go-enjin/be/pkg/log"
-	"github.com/go-enjin/be/pkg/maps"
+	"github.com/go-enjin/be/pkg/pages"
 )
 
 func (re *RenderEnjin) ParseFieldAttributes(field map[string]interface{}) (attributes map[string]interface{}, classes []string, styles map[string]string, ok bool) {
 	var err error
-	if attributes, classes, styles, err = maps.ParseNjnFieldAttributes(field); err != nil {
+	if attributes, classes, styles, err = pages.ParseNjnFieldAttributes(field); err != nil {
 		log.ErrorF("error parsing njn field attributes: %v", err)
 		ok = false
 		return
@@ -34,7 +34,7 @@ func (re *RenderEnjin) ParseFieldAttributes(field map[string]interface{}) (attri
 }
 
 func (re *RenderEnjin) FinalizeFieldData(data map[string]interface{}, field map[string]interface{}, skip ...string) {
-	if err := maps.FinalizeNjnFieldData(data, field, skip...); err != nil {
+	if err := pages.FinalizeNjnFieldData(data, field, skip...); err != nil {
 		log.ErrorF("error finalizing njn field data: %v", err)
 	}
 	return
@@ -42,7 +42,7 @@ func (re *RenderEnjin) FinalizeFieldData(data map[string]interface{}, field map[
 
 func (re *RenderEnjin) FinalizeFieldAttributes(attrs map[string]interface{}) (attributes []template.HTMLAttr) {
 	var err error
-	if attributes, err = maps.FinalizeNjnFieldAttributes(attrs); err != nil {
+	if attributes, err = pages.FinalizeNjnFieldAttributes(attrs); err != nil {
 		log.ErrorF("error finalizing njn field attributes: %v", err)
 	}
 	return
